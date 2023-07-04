365072718fd513b82d3e935e7324bec4a7cb92fa
Muhammad Shakib Hossain <>
2023-07-04 10:04:10 +0600

    image added footer social icons

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,20 @@
+import Image from 'next/image';
+import Hero from 'public/hero.png';
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div className="flex items-center gap-[100px] ">
+      <div className="flex-1">
+        <h1>Better design for your digital products</h1>
+        <p>
+          Tuning your Idea into Reality. We bring together the teams from the
+          global tech industry
+        </p>
+        <button>See Our Work</button>
+      </div>
+      <div>
+        <Image src={Hero} alt="" className="" />
+      </div>
+    </div>
+  );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,42 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="h-[50px] flex items-center justify-between">
+    <div className="h-[50px] text-[14px] flex items-center justify-between">
       <div>2023 Soft Dazzle. All rights reserved.</div>
-      <div></div>
+      <div>
+        <div className="flex items-center gap-[10px]">
+          <Image
+            src="/1.png"
+            width={15}
+            height={15}
+            className="opacity-[0.6] cursor-pointer"
+            alt="Soft Dazzle"
+          />
+          <Image
+            src="/2.png"
+            width={15}
+            height={15}
+            className="opacity-[0.6] cursor-pointer"
+            alt="Soft Dazzle"
+          />
+          <Image
+            src="/3.png"
+            width={15}
+            height={15}
+            className="opacity-[0.6] cursor-pointer"
+            alt="Soft Dazzle"
+          />
+          <Image
+            src="/4.png"
+            width={15}
+            height={15}
+            className="opacity-[0.6] cursor-pointer"
+            alt="Soft Dazzle"
+          />
+        </div>
+      </div>
     </div>
   );
 };
