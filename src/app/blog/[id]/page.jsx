93cccd1343154675337ca9c22a75dde14e9c7f29
Muhammad Shakib Hossain <>
2023-07-04 18:46:09 +0600

    blog

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import styles from './page.module.css';
+import Image from 'next/image';
 
-const BlogPost = () => {
-  return <div>BlogPost</div>;
+const BlogPost = async () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>Hello Bangladesh</h1>
+          <p className={styles.desc}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore
+            quasi illum omnis quia fugit repellat!
+          </p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/2295744/pexels-photo-2295744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Hello World</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/2295744/pexels-photo-2295744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Reprehenderit nemo obcaecati rerum sapiente consequatur reiciendis
+          corrupti, illo accusantium, tempora alias delectus fugiat quaerat
+          deserunt molestiae soluta necessitatibus ipsa vero similique? Repellat
+          quod quae commodi tenetur natus sapiente, illum, incidunt assumenda
+          ipsam consequatur recusandae ex earum! Dignissimos dolorem harum
+          nihil? Consequatur?
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,66 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="">
+      <Link
+        className="flex items-center gap-[50px] mb-[50px]"
+        href="/blog/testId"
+      >
+        <div>
+          <Image
+            src="https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={400}
+            height={250}
+            className=""
+          />
+        </div>
+        <div className="">
+          <h1 className="">Test</h1>
+          <p className="">Desc</p>
+        </div>
+      </Link>
+      <Link
+        className="flex items-center gap-[50px] mb-[50px]"
+        href="/blog/testId"
+      >
+        <div>
+          <Image
+            src="https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={400}
+            height={250}
+            className=""
+          />
+        </div>
+        <div className="">
+          <h1 className="">Test</h1>
+          <p className="">Desc</p>
+        </div>
+      </Link>
+      <Link
+        href="/blog/testId"
+        className="flex items-center gap-[50px] mb-[50px]"
+      >
+        <div>
+          <Image
+            src="https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={400}
+            height={250}
+            className=""
+          />
+        </div>
+        <div className="">
+          <h1 className="">Test</h1>
+          <p className="">Desc</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Blog;
